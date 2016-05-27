4caaeebbe16811277bf54eb78433c3de78fa960a
Marcin Szpaderski <szpaderskimarcin@gmail.com>
2016-05-27 15:33:18 +0200

    Connecting user to comments tryout 3

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-    belongs_to :user
+    belongs_to :user, :as => :created_by
     has_many :comments
     validates :title, presence: true, length: {maximum:50}
     validates :post, presence: true
