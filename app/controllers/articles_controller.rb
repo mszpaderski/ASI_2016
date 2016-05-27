fae3afcf587992310a99b1fe06e92b17f0d5283b
Marcin Szpaderski <szpaderskimarcin@gmail.com>
2016-05-27 16:30:24 +0200

    Connecting user to comments tryout 7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -70,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-        params.require(:article).permit(:title, :players_lvl, :post)
+        params.require(:article).permit(:title, :players_lvl, :post, :user_id)
     end
 end
